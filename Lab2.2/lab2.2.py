ca9c63a504f350a9fe8468b975a1f1f9bb7c0e0d
EnjiRouz <enji.rouz@yandex.ru>
2019-10-21 20:21:25 +0500

    Done lab 2.2

--- Lab2.2/lab2.2.py
+++ Lab2.2/lab2.2.py
@@ -3,6 +3,13 @@ TASK:
 Разработайте экспертную систему
 продукционного типа с обратным логическим выводом
 
+Если двигатель не заводится, и фары не горят, то сел аккумулятор.
+Если засорился бензонасос, то двигатель не заводится.
+Если указатель бензина находится на нуле, то двигатель не заводится.
+Если указатель бензина находится на нуле, то нет бензина.
+
+Имеют место факты: фары не горят, указатель бензина находится на нуле
+
 EXAMPLES:
 сел аккумулятор
 '''
@@ -31,14 +38,40 @@ def get_last_element(user_list):
     return user_list[-1]
 
 
+# находит возможные причины проблем (составляет объяснения)
+def find_explanation(fact):
+    reasons = facts.get(fact)
+    if len(reasons) >= 1:
+        print("\nДля проблемы " + fact + " характерны следующие причины:")
+        for reason in reasons:
+            print("* " + reason)
+        for reason in reasons:
+            if reason in facts:
+                find_explanation(reason)
+
+
 if __name__ == '__main__':
     while True:
-        # выводы, сделанные на основе фактов
-        conclusion = []
-
         # факты, которые вводит пользователь
         user_facts = str(input('\nTell me a fact. Example input:\nсел аккумулятор\n')) \
             .split(", ")
-    #длинная цепочка всяких фактов :0
 
-    conclusion.clear()
+        known_facts = list(facts.keys())
+
+        # удаляем повторяющиеся факты
+        for user_fact in user_facts:
+            while user_facts.count(user_fact) > 1:
+                user_facts.remove(user_fact)
+
+        # проверяем, содержат ли факты пользователя известные нашей базе факты, и удаляем лишние
+        existing_facts = []
+        for user_fact in user_facts:
+            if user_fact in known_facts:
+                existing_facts.append(user_fact)
+        user_facts = existing_facts
+
+        # рекурсионный вызов для поиска объяснения
+        if len(user_facts) >= 1:
+            for user_fact in user_facts:
+                print("\nОбъяснение проблемы " + user_fact, end=":")
+                find_explanation(user_fact)
