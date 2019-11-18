79c0810aaa4a2bd12153ab7e3ba2d8399a6f01f0
EnjiRouz <enji.rouz@yandex.ru>
2019-11-18 18:18:10 +0500

    Started working with Neural Networks

---
+++ NeuralNetwork/main.py
@@ -0,0 +1,59 @@
+# Imports
+import numpy as np
+
+# Each row is a training example, each column is a feature  [X1, X2, X3]
+X = np.array(([0, 0, 1], [0, 1, 1], [1, 0, 1], [1, 1, 1]), dtype=float)
+y = np.array(([0], [1], [1], [0]), dtype=float)
+
+
+# Define useful functions
+
+# Activation function
+def sigmoid(t):
+    return 1 / (1 + np.exp(-t))
+
+
+# Derivative of sigmoid
+def sigmoid_derivative(p):
+    return p * (1 - p)
+
+
+# Class definition
+class NeuralNetwork:
+    def __init__(self, input_layer, output_layer):
+        self.input = input_layer
+        self.weights1 = np.random.rand(self.input.shape[1], 4)  # considering we have 4 nodes in the hidden layer
+        self.weights2 = np.random.rand(4, 1)
+        self.y = output_layer
+        self.output = np.zeros(output_layer.shape)
+
+    def feedforward(self):
+        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
+        self.layer2 = sigmoid(np.dot(self.layer1, self.weights2))
+        return self.layer2
+
+    def back_propagation(self):
+        d_weights2 = np.dot(self.layer1.T, 2 * (self.y - self.output) * sigmoid_derivative(self.output))
+        d_weights1 = np.dot(self.input.T, np.dot(2 * (self.y - self.output) * sigmoid_derivative(self.output),
+                                                 self.weights2.T) * sigmoid_derivative(self.layer1))
+
+        self.weights1 += d_weights1
+        self.weights2 += d_weights2
+
+    def train(self):
+        self.output = self.feedforward()
+        self.back_propagation()
+
+
+NN = NeuralNetwork(X, y)
+for i in range(1500):  # trains the NN 1,000 times
+    if i % 100 == 0:
+        print("for iteration # " + str(i) + "\n")
+        print("Input : \n" + str(X))
+        print("Actual Output: \n" + str(y))
+        print("Predicted Output: \n" + str(NN.feedforward()))
+        print("Loss: \n" + str(np.mean(np.square(y - NN.feedforward()))))  # mean sum squared loss
+        print("Weights:\n"+str(NN.weights1)+", "+str(NN.weights2))
+        print("\n")
+
+    NN.train()
