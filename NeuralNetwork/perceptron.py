8dbf06e0ede31d594109a661ee50b2d76dc71698
EnjiRouz <enji.rouz@yandex.ru>
2019-11-25 15:01:27 +0500

    Improved perceptron output

--- NeuralNetwork/perceptron.py
+++ NeuralNetwork/perceptron.py
@@ -16,7 +16,7 @@ class Perceptron(object):
         return activation
 
     def train(self, training_inputs, labels):
-        for _ in range(self.threshold):
+        for i in range(self.threshold):
             for inputs, label in zip(training_inputs, labels):
                 prediction = self.predict(inputs)
                 self.weights[1:] += self.learning_rate * (label - prediction) * inputs
@@ -25,14 +25,17 @@ class Perceptron(object):
 
 if __name__ == '__main__':
     training_inputs = [np.array([1, 1]), np.array([1, 0]), np.array([0, 1]), np.array([0, 0])]
-    labels = np.array([1, 0, 0, 0])
+    expected_output = np.array([1, 0, 0, 0])
 
     perceptron = Perceptron(2)
     print("Initial weights: "+str(perceptron.weights))
 
-    perceptron.train(training_inputs, labels)
+    perceptron.train(training_inputs, expected_output)
     print("Prediction for [1, 1]: "+str(perceptron.predict(np.array([1, 1]))))
+    print("Prediction for [1, 0]: "+str(perceptron.predict(np.array([1, 0]))))
     print("Prediction for [0, 1]: "+str(perceptron.predict(np.array([0, 1]))))
+    print("Prediction for [0, 0]: "+str(perceptron.predict(np.array([0, 0]))))
+
     print("Final weights: "+str(perceptron.weights))
 
     training_inputs.clear()
